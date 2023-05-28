5d0cd40793bfc2e7433a30711e5bb1f6fe2bedf4
deveshbadolia <124484199+deveshbadolia@users.noreply.github.com>
2023-05-28 15:06:44 +0530

    Update twitter_dag.py

--- twitter_dag.py
+++ twitter_dag.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
-from twitter_etl import run_twitter_etl
+from twitter import run_twitter_etl
 
 default_args = {
     'owner': 'airflow',
