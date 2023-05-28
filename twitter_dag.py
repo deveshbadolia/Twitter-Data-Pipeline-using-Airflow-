8215bc842ae9021e786f1a33029af845edd25ed9
deveshbadolia <124484199+deveshbadolia@users.noreply.github.com>
2023-05-28 15:05:26 +0530

    Update twitter_dag.py

--- twitter_dag.py
+++ twitter_dag.py
@@ -8,7 +8,7 @@ from twitter_etl import run_twitter_etl
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
+    'start_date': datetime(2023, 1, 1),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -19,7 +19,7 @@ default_args = {
 dag = DAG(
     'twitter_dag',
     default_args=default_args,
-    description='Our first DAG with ETL process!',
+    description='Twitter Data Pipeline',
     schedule_interval=timedelta(days=1),
 )
 
